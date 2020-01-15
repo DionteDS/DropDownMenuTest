b5dfda0c54cc902b1b9c91560aa572dcccccbfb0
Dionte <Dionte_sb@yahoo.com>
2020-01-15 13:25:09 -0800

    Animated the drop down menu.

--- DropDownMenuTest/ViewController.swift
+++ DropDownMenuTest/ViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
+enum Cities: String {
+    case sanFrancisco = "San Francisco"
+    case newYorkCity = "NewYorkCity"
+    case losAngeles = "Los Angeles"
+    case chicago = "Chicago"
+    case dallas = "Dalls"
+}
+
 class ViewController: UIViewController {
     
+    @IBOutlet var ctiyButtons: [UIButton]!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "Select a city"
+        navigationController?.navigationBar.barTintColor = UIColor.darkGray
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        view.backgroundColor = UIColor.darkGray
+        
+    }
+    
+    @IBAction func cityHandler(_ sender: UIButton) {
+        
+        ctiyButtons.forEach { (button) in
+            UIView.animate(withDuration: 0.3) {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+    
+    @IBAction func cityTapped(_ sender: UIButton) {
+        
+        
         
     }
 
